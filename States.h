53c95742d81d81e83b6120646a7712dbcc2a0f18
Rahul Dev E <rahul.e.dev@gmail.com>
2020-02-08 11:59:04 +0530

    Update States.h

--- States.h
+++ States.h
@@ -1,23 +1,7 @@
+#pragma once
 #include "Ri.h"
+#include "Primitives.h"
 #include <Eigen/Eigen>
-
-class CameraState {
-public:
-	// Related to the times at which the shutter opens and closes
-	// https://renderman.pixar.com/resources/RenderMan_20/options.html#rishutter
-	float shutter_min;
-	float shutter_max;
-};
-
-class GraphicState {
-public:
-	// Set by RiColor
-	RtColor color;
-
-	// Set by RiPixelSamples
-	// https://renderman.pixar.com/resources/RenderMan_20/options.html#ripixelsamples
-	float xsamples, ysamples;
-};
 
 class ImageState {
 public:
@@ -28,10 +12,56 @@ public:
 	ImageState();
 };
 
-class FrameState {
+class CameraState {
 public:
+	// Related to the times at which the shutter opens and closes
+	// https://renderman.pixar.com/resources/RenderMan_20/options.html#rishutter
+	float shutter_min;
+	float shutter_max;
+};
+
+class TransformationState {
+public:
+	// This represents the current matrix used for model-to-world transformation
+	// Defaults to Identity
+	// Set using Ri* transformation functions when inside an RiTRansformBegin block
+	Eigen::Matrix4f current_transformation;
+	int is_in_transform_block;
+
+	TransformationState();
+};
+
+class WorldState {
+public:
+	std::vector<Primitive> objects;
+	std::vector<Eigen::Vector4f> world_mesh;
+};
+
+class RenderState {
+public:
+
+	//RtToken projection_type;
+	float zNear, zFar;
+
+	// https://renderman.pixar.com/resources/RenderMan_20/attributes.html#ricolor
+	Color current_color;
+	// Set by RiOpacity
+
+	// Set by RiPixelSamples
+	// https://renderman.pixar.com/resources/RenderMan_20/options.html#ripixelsamples
+	float xsamples, ysamples;
+
+	// https://renderman.pixar.com/resources/RenderMan_20/options.html#camera
+	// If it is not set by RiFrameAspectRatio, then it is calculated using ImageState
+	float frame_aspect_ratio;
+
 	// https://renderman.pixar.com/resources/RenderMan_20/graphicsState.html#ribegin
 	int f_no;
-	Eigen::Matrix4f current_transformation;
-	FrameState();
+	int is_in_world_block;
+
+	// This matrix represent world-to-camera and camera-to-screen projection combined
+	Eigen::Matrix4f transformation;
+
+	//std::vector<Eigen::Vector4f> world_mesh;
+	RenderState();
 };
