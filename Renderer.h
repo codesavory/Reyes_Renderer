a545dc6e9fa2d36aa42eec9df7cda92d79368717
Rahul Dev E <rahul.e.dev@gmail.com>
2020-02-13 13:54:16 +0530

    Update Renderer.h

--- Renderer.h
+++ Renderer.h
@@ -7,6 +7,7 @@
 #include <Eigen/Eigen>
 #include <iostream>
 #include <numeric>
+#include "Buffers.h"
 
 inline
 float edgeFunction(const Eigen::Vector4f& a, const Eigen::Vector4f& b, const Eigen::Vector4f& c)
@@ -15,92 +16,7 @@ float edgeFunction(const Eigen::Vector4f& a, const Eigen::Vector4f& b, const Eig
 }
 
 
-template <class T>
-class Buffer {
-public:
-    int w;
-    int h;
-    int xsamples, ysamples;
-    std::vector<T> buff;
 
-    Buffer(int width, int heigth, int xsamples, int ysamples) {
-        this->w = width;
-        this -> h = heigth;
-        this->xsamples = xsamples;
-        this->ysamples = ysamples;
-        buff.resize(width * xsamples * heigth * ysamples);
-        //buff.resize(width * heigth);
-    }
-
-    T operator() (int x, int y, int m, int n) const {
-        int id = (y * xsamples * ysamples * w + x * xsamples * ysamples + n * xsamples + m);
-        //int id = (y * xsamples * ysamples * w + x * xsamples * ysamples + 0);
-        //int id = y * w + x;
-        return buff[id];
-    }
-
-    T& operator() (int x, int y, int m, int n) {
-        int id = (y * xsamples * ysamples * w + x * xsamples * ysamples + n * xsamples + m);
-        //int id = (y * xsamples * ysamples * w + x * xsamples * ysamples + 0);
-        //int id = y * w + x;
-        return buff[id];
-    }
-};
-
-
-class FrameBuffer : public Buffer<Color> {
-public:
-    FrameBuffer(int width, int heigth, int xsamples, int ysamples)
-        : Buffer(width, heigth, xsamples, ysamples) {}
-
-    Color collapse(int x, int y) {
-
-        Eigen::Vector4f pix_color = Eigen::Vector4f::Zero();
-
-        for (int m = 0; m < xsamples; ++m) {
-            for (int n = 0; n < ysamples; ++n) {
-                Color sc = this->operator()(x, y, m, n);
-                pix_color += Eigen::Vector4f(sc.r, sc.g, sc.b, sc.a);
-            }
-        }
-
-        pix_color /= (xsamples * ysamples);
-        Color c(pix_color[0], pix_color[1], pix_color[2], pix_color[3]);
-        return c;
-    }
-};
-
-
-
-class ZBuffer : public Buffer<std::list<float>> {
-public:
-    ZBuffer(int width, int heigth, int xsamples, int ysamples)
-        : Buffer(width, heigth, xsamples, ysamples) 
-    {}
-
-    std::list<float> operator() (int x, int y, int m, int n) const {
-        std::list<float> buff_at_loc = Buffer<std::list<float>>::operator()(x, y, m, n);
-        if (buff_at_loc.empty()) {
-            buff_at_loc.push_front(std::numeric_limits<float>::infinity());
-            return buff_at_loc;
-        }
-        else {
-            return buff_at_loc;
-        }
-    }
-
-    std::list<float>& operator() (int x, int y, int m, int n) {
-        std::list<float>& buff_at_loc = Buffer<std::list<float>>::operator()(x, y, m, n);
-        if (buff_at_loc.empty()) {
-            buff_at_loc.push_front(std::numeric_limits<float>::infinity());
-            return buff_at_loc;
-        }
-        else {
-            return buff_at_loc;
-        }
-    }
-
-};
 
 
 inline
@@ -153,7 +69,9 @@ void sample(int x, int y, int xsamples, int ysamples, FrameBuffer& fb, ZBuffer& 
                     float g = w0 * c[0].g + w1 * c[1].g + w2 * c[2].g;
                     float b = w0 * c[0].b + w1 * c[1].b + w2 * c[2].b;
                     Color z(r, g, b, 1.0);
-                    fb(x, y, m, n) = t.colors[0];
+                    fb(x, y, m, n) = z;
+
+                    
 
 
                 }
@@ -219,8 +137,8 @@ void render_frame(WorldState& world_state, RenderState& render_state, ImageState
 	const int height = image_state.y_resolution;
 
 	// responsible for storing color
-	FrameBuffer frame_buffer(width, height, 2, 2);
-    ZBuffer z_buffer(width, height, 2, 2);
+	FrameBuffer frame_buffer(width, height, xsamples, ysamples);
+    ZBuffer z_buffer(width, height, xsamples, ysamples);
 
     std::vector<std::unique_ptr<Primitive>>& world_obj_ptrs = world_state.object_ptrs;
 
@@ -250,6 +168,8 @@ void render_frame(WorldState& world_state, RenderState& render_state, ImageState
             p.y() = screen_y;
 
 			points[j] = p;
+
+            //frame_buffer(p.x(), p.y(), 0, 0) = Color(255.0f, 255.0f, 255.0f, 1.0f);
 		}
         
 
@@ -303,14 +223,6 @@ void render_frame(WorldState& world_state, RenderState& render_state, ImageState
                     tri.colors[0] = objPtr->vertex_colors[triangle_vert_ids[0]];
                     tri.colors[1] = objPtr->vertex_colors[triangle_vert_ids[1]];
                     tri.colors[2] = objPtr->vertex_colors[triangle_vert_ids[2]];
-
-                    tri.u_vals[0] = objPtr->u_vals[triangle_vert_ids[0]];
-                    tri.u_vals[1] = objPtr->u_vals[triangle_vert_ids[1]];
-                    tri.u_vals[2] = objPtr->u_vals[triangle_vert_ids[2]];
-
-                    tri.v_vals[0] = objPtr->v_vals[triangle_vert_ids[0]];
-                    tri.v_vals[1] = objPtr->v_vals[triangle_vert_ids[1]];
-                    tri.v_vals[2] = objPtr->v_vals[triangle_vert_ids[2]];
 
                     sample(x, y, xsamples, ysamples, frame_buffer, z_buffer, tri);   
                 }
